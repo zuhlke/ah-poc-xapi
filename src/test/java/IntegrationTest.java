a4395f7cc75d0cea22a9eea4f2fbfed064f9df2e
romo <robert.moore@zuhlke.com>
2019-06-05 09:55:31 +0100

    Improve the integration test

--- src/test/java/IntegrationTest.java
+++ src/test/java/IntegrationTest.java
@@ -1,9 +1,14 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import org.junit.After;
 import org.junit.Test;
 import reactor.core.publisher.Mono;
+
+import java.io.IOException;
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -12,7 +17,7 @@ import static org.mockito.Mockito.when;
 
 public class IntegrationTest {
     private final int portNumber = 9090;
-    private final String origin = "http://localhost:" + portNumber;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @After
     public void tearDown() {
@@ -29,18 +34,24 @@ public class IntegrationTest {
     }
 
     @Test
-    public void GET_sumBalances_returnsSumOfBalancesFromPapi() throws UnirestException {
+    public void GET_sumBalances_returnsSumOfBalancesFromPapi() throws UnirestException, IOException {
         PapiService stubPapiService = mock(PapiService.class);
         when(stubPapiService.sumBalances("1")).thenReturn(Mono.just(1234.0));
         SpringController.startWithInjectedPapiService(portNumber, stubPapiService);
 
         String responseText = getRequestText("/sum-balances?customer-id=1");
 
-        String expectedResponse = "{\"sumBalance\": \"1234.0\"}";
-        assertThat(responseText, equalTo(expectedResponse));
+        Map<String, Double> map = readSumBalanceJsonResponseToMap(responseText);
+        assertThat(map.get("sumBalance"), equalTo(1234.0));
+    }
+
+    private Map<String, Double> readSumBalanceJsonResponseToMap(String responseText) throws IOException {
+        return objectMapper.readValue(responseText, new TypeReference<Map<String, Double>>() {
+        });
     }
 
     private String getRequestText(String requestPath) throws UnirestException {
+        String origin = "http://localhost:" + portNumber;
         String url = origin + requestPath;
         HttpResponse<String> response = Unirest.get(url).asString();
         return response.getBody();
