ec38ecf87c0d632ab1821d2c8f96c79bb8f9e96a
romo <robert.moore@zuhlke.com>
2019-06-05 16:05:02 +0100

    Create a consumer-side pact test on the XAPI. Plus documentation, and checked in the pact file itself as well.

--- README.md
+++ README.md
@@ -14,3 +14,22 @@ then you'll get a response body like
 ```
 {"accountNumber": "10101010", "sumBalance": "1269.0"}
 ```
+
+## Pact
+
+There is a pact test, `ReactivePapiServicePact`. The pact guidelines suggest writing your pact test as a test against your interface for
+interacting with the provider. Therefore this test is effectively a component test against the `ReactivePapiService` and
+`ReactiveRestClient`.
+
+- Perhaps it should just be a unit test against the `ReactiveRestClient`.
+
+When you run the pact test, a pact file is created in `target/pacts/` with the naming convention `<consumer_name>-<provider_name>.json`.
+The names of the consumer and provider are given in the test as overriden functions against the pact testing framework's API. I have
+added comments in the test class itself.
+
+#### Next steps
+
+1. Verify the contract in the consumer pipeline
+2. Run a pact broker on PCF
+3. Upload generated pacts to the pact broker
+4. Run pacts from the pact broker within the provider's pipeline
---
+++ src/test/java/ReactivePapiServicePact.java
@@ -0,0 +1,70 @@
+import au.com.dius.pact.consumer.ConsumerPactTestMk2;
+import au.com.dius.pact.consumer.MockServer;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.model.RequestResponsePact;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ReactivePapiServicePact extends ConsumerPactTestMk2 {
+    @Override
+    protected RequestResponsePact createPact(PactDslWithProvider builder) {
+        /*
+        This section of code is used by pact to produce the main body of the pact file.
+        The stubbed behaviour defined here is the standard that the provider, which this code imitates, is held to by the pact.
+        */
+
+        String stubbedResponseBody = "[{\"accountType\": \"CreditCardAccount\",\"accountNumber\": \"1234567890\",\"balance\": \"1234.50\"},{\"accountType\": \"CurrentAccount\",\"accountNumber\": \"64746383648\",\"balance\": \"34.50\"}]";
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", MediaType.APPLICATION_JSON_VALUE);
+
+        return builder
+                .given("The PAPI is backed by two working SAPIs, each able to serve account data for a customer with id 1")
+                .uponReceiving("A request for a list of balances for a customer with id 1")
+                .path("/reactive-balance")
+                .query("customer-id=1")
+                .method("GET")
+                .willRespondWith()
+                .status(200)
+                .headers(headers)
+                .body(stubbedResponseBody)
+                .toPact();
+    }
+
+    @Override
+    protected void runTest(MockServer mockServer) {
+        int port = mockServer.getPort();
+        String stubPapiOrigin = "http://localhost:" + port;
+        ReactiveRestClient reactiveRestClient = new ReactiveRestClient(WebClient.create());
+        ReactivePapiService reactivePapiService = new ReactivePapiService(reactiveRestClient, stubPapiOrigin + "/reactive-balance?customer-id={CUSTOMER_ID}");
+
+        assertThatItCanSumAccountBalances(reactivePapiService);
+        /*
+        For testing more interactions with the PAPI, I believe you would put more assertions here. That is at least what I've seen from the working example.
+
+        Working Example: https://github.com/zuhlke/pact-consumer/blob/master/src/test/java/com/zuhlke/report/service/ConsumerTest.java
+        */
+    }
+
+    private void assertThatItCanSumAccountBalances(ReactivePapiService reactivePapiService) {
+        Mono<Double> sumBalances = reactivePapiService.sumBalances("1");
+
+        assertThat(sumBalances.block(), equalTo(1269.0));
+    }
+
+    @Override
+    protected String providerName() {
+        return "ah-poc-papi-springboot";
+    }
+
+    @Override
+    protected String consumerName() {
+        return "ah-poc-xapi";
+    }
+}
